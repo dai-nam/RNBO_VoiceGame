33aad63bac7d085a8ef72e05be6cfb565170db53
KOPRO-WORK-2\kopro <michael@koprodo.de>
2024-02-23 16:03:03 +0100

    Implemented p5 Game logic

--- js/mysketch.js
+++ js/mysketch.js
@@ -2,18 +2,24 @@
 var frequency = 0;
 var amplitude = 0;
 
-var x = -100;
-var y = -100;
-var scaledX;
-var scaledY;
+var lowerFreqBound = 90;
+var upperFreqBound = 300;
 
-var lowerFreqBound = 100;
-var upperFreqBound = 600;
+var lowerAmpBound = 0.1;
+var upperAmpBound = 0.4;
 
-var lowerAmpBound = 0.0;
-var upperAmpBound = 1.0;
+var easing = 0.008;
 
-var easing = 0.03;
+
+var target = null;
+var targetSize = 150;
+var score = 0;
+var aim;
+var loadTime = 1000;
+let playAreaHeight;
+let textBarHeight = 30;
+let ypadding = 10;
+
 
 //-------------------------rnbo-----------------------------
 
@@ -26,6 +32,7 @@ let mediarecorder;
 let chunks;
 let audioRecording;
 let isRecording = false;
+
 
 async function loadRnbo( ){
 
@@ -66,50 +73,7 @@ async function startAudio()
   console.log("Audio started. Samplerate: "+context.sampleRate);
 }
 
-// -----------------------p5--------------------------
-
-
-function setup() {
-    createCanvas(800, 800);
-    createButtons();
-}
-
-
-function createButtons() {
-  var yoffset = 20;
-
-  let button1 = createButton('Record');
-  button1.position(0, height + yoffset);
-
-  let button2 = createButton('Stop Mic');
-  button2.position(200, height + yoffset);
-
-  let button3 = createButton('Play');
-  button3.position(400, height + yoffset);
-
-  let gainSlider = createSlider(0, 300);
-  gainSlider.position(600, height+yoffset);
-  gainSlider.size(100);
- // gainSlider.value = 100;
-
-  button1.mousePressed(() => {
-    recordIntoBuffer();
-  });
-
-  button2.mousePressed(() => {
-    stopMicrophone();
-  });
-
-  button3.mousePressed(() => {
-    audioRecording.play();
-  });
-
-  gainSlider.input(() => {
-    var val = map(gainSlider.value(), 0, 300, 0.0, 3.0, true)
-    gainNode.gain.value = val;
-  });
-}
-
+//----------------------Web Audio API------------------------
 async function recordIntoBuffer()
 {
   if(isRecording)
@@ -117,7 +81,7 @@ async function recordIntoBuffer()
     return;
   }
 
-  console.log("Recording started");
+  console.log("Game started");
 
   isRecording = true;
   await startMicrophone();
@@ -140,6 +104,7 @@ async function startMicrophone()
     
     //mic input as an audionode
     const mic = context.createMediaStreamSource(stream);
+
     let audioDest = context.createMediaStreamDestination();
     mic.connect(gainNode);
     gainNode.connect(audioDest);
@@ -158,7 +123,7 @@ async function stopMicrophone()
     return;
   }
 
-  console.log("Recording stopped");
+  console.log("Game stopped");
   isRecording = false;
   mediaRecorder.stop();
 
@@ -181,11 +146,81 @@ async function stopMicrophone()
 }
 
 
-function draw() {
-    background(255, 0 , 0);
-    display();
+// -----------------------p5--------------------------
+
+
+function setup() {
+    createCanvas(800, 800);
+    createButtons();
+
+    playAreaHeight = height-textBarHeight;
+
+    spawnNewTarget();
+    aim = new Aim(-100, -100);
 }
 
+function displayTextBar()
+{
+  strokeWeight(1);
+  stroke(0);
+  fill(255);
+  rect(0, height-textBarHeight, width, textBarHeight);
+
+  fill(0);
+  textSize(20);
+  text("Score: "+score, 100, height-ypadding);
+}
+
+
+function createButtons() {
+  var yoffset = 18;
+
+  let button1 = createButton('Play');
+  button1.position(300, height - yoffset);
+
+  let button2 = createButton('Stop');
+  button2.position(400, height - yoffset);
+
+  let button3 = createButton('Play Recording');
+  button3.position(500, height- yoffset );
+
+  let gainSlider = createSlider(0, 300);
+  gainSlider.position(600, height- yoffset);
+  gainSlider.size(100);
+ // gainSlider.value = 100;
+
+  button1.mousePressed(() => {
+    recordIntoBuffer();
+  });
+
+  button2.mousePressed(() => {
+    stopMicrophone();
+  });
+
+  button3.mousePressed(() => {
+    audioRecording.play();
+  });
+
+  gainSlider.input(() => {
+    var val = map(gainSlider.value(), 0, 300, 0.0, 3.0, true)
+    gainNode.gain.value = val;
+  });
+}
+
+
+function draw() {
+    background(220);
+    displayTextBar();
+    target.display();
+    aim.updatePosition();
+    aim.display();
+    aim.checkIfTargetHit();
+  }
+
+function spawnNewTarget()
+{
+    target = new Target(random(targetSize, width-targetSize), random(targetSize, playAreaHeight-targetSize), targetSize);
+}
 
 function mousePressed()
 {
@@ -193,26 +228,149 @@ function mousePressed()
 }
 
 
-function display()
+
+function mouseInBounds()
 {
-  scaledX = map(frequency, lowerFreqBound, upperFreqBound, 0, width, true);
-  scaledY = map(amplitude, lowerAmpBound, upperAmpBound, 0, height, true);
+  return mouseX >= 0 && mouseX < width && mouseY >= 0 && mouseY < playAreaHeight;
+}
 
-  let targetX = scaledX;
-  let dx = targetX - x;
-  x += dx * easing;
+  
+class Aim{
 
-  let targetY = scaledY;
-  let dy = targetY - y;
-  y += dy * easing;
+  constructor(x, y)
+  {
+    this.x = x;
+    this.y = y;
+    this.rad = 20;
+    this.timeSinceLastHit = 0;
 
-  fill(0,0,255);
-  circle(x, y, 20);
- // console.log(x, +"       "+y);
+  }
+
+  updatePosition()
+  {
+    if(mouseIsPressed && mouseInBounds())
+    {
+      this.x = mouseX;
+      this.y = mouseY;
+    }
+    else
+    {
+      let scaledX = map(frequency, lowerFreqBound, upperFreqBound, 0, width, true);
+      let scaledY = map(amplitude, lowerAmpBound, upperAmpBound, 0, playAreaHeight, true);
+
+      let targetX = scaledX;
+      let dx = targetX - this.x;
+      this.x += dx * easing;
+    
+      let targetY = scaledY;
+      let dy = targetY - this.y;
+      this.y += dy * easing;
+
+      console.log(this.x+"        "+this.y);
+
+    }
+  }
+  
+  display()
+  {
+    fill(0);
+    circle(this.x, this.y, this.rad);
+  }
+  
+  checkIfTargetHit()
+  {
+    var d = dist(this.x, this.y, target.x, target.y);
+    if(d < target.rad/2)
+    {
+      this.timeSinceLastHit += deltaTime;
+      target.loading(this.timeSinceLastHit);
+      
+      if(this.timeSinceLastHit >= loadTime)
+      {
+        this.timeSinceLastHit = 0;
+        score++;
+        console.log("Hit Target; score: "+score);
+        spawnNewTarget();
+      }
+      
+    }
+    else
+    {
+        this.timeSinceLastHit = 0;
+        target.loadingCircles = [];
+    }   
+  }
 }
 
 
 
+class Target{
+  
+  constructor(x, y, r){
+    this.x = x;
+    this.y = y;
+    this.rad = r
+    this.loadingCircles = [];
+    this.c = color(random(255), random(255), random(255));
+  }
+  
+  
+  display()
+  {
+    noStroke();
+    fill(this.c);
+    circle(this.x, this.y, this.rad);
+    
+    for(let lc of this.loadingCircles)
+      {
+        lc.display();
+      }
+  }
+  
+  loading(timePassed)
+  {
+    var lc = new LoadingCircle(this.x, this.y, timePassed, this.rad);
+    this.loadingCircles.push(lc); 
+  }
+}
+
+
+class LoadingCircle
+{
+    constructor(x, y, timePassed, targetRad){
+    this.x = x;
+    this.y = y;
+    this.rad = 10
+    this.c = color(255, 0, 0);
+    this.offset = this.calculateOffset(timePassed);
+    this.targetRad = targetRad;
+  }
+    
+    display()
+    {
+      fill(this.c);
+      let pos = createVector(this.x, this.y);
+      this.offset.setMag(this.targetRad/2);
+      pos.add(this.offset);
+      circle(pos.x, pos.y, this.rad);
+    }
+  
+    calculateOffset(timePassed)
+  {
+    var progress = timePassed/loadTime;
+    var vec = this.calculateClockwiseVector(progress);
+    return vec;
+  }
+  
+  calculateClockwiseVector(t) 
+  {
+    let radians = t * TWO_PI -HALF_PI;
+    let x = cos(radians);
+    let y = sin(radians); 
+    return createVector(x, y);
+  }
+  
+}
 
 
 
